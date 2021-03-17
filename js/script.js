2f45e14de78cc53c5f5f6deb3521f708e2185b45
Valentina <aveatikk@gmail.com>
2021-03-17 08:25:20 +0500

    wip

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ document.querySelector('.burger').addEventListener('click', function(){
     document.querySelector('.menu').classList.toggle("animate");
 })
 
+const elements = document.querySelectorAll('.menu-text')
+    for ( let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('click', function() {
+            document.querySelector('.menu').classList.toggle('animate');
+            document.querySelector('.burger span').classList.toggle('active');
+        })
+    }
+   
+
+
